8eb0f3cfc424574cbd814aae27ee154006a5d3de
Heinrich Kuhn <heinrich.kuhn@netronome.com>
2017-08-09 09:13:46 +0200

    Add Agilio-OVS 2.6B IVG scripts

--- performance_configuration/1_configure_grub.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-OUT="$(grep -n "GRUB_CMDLINE_LINUX_DEFAULT" /etc/default/grub | cut -d: -f1)"
-sed -i "$OUT s/^/#/" /etc/default/grub
-
-sed -i "$OUT a GRUB_CMDLINE_LINUX_DEFAULT=\"intel_iommu=on iommu=pt intremap=on isolcpus=1-10 intel_idle.max_cstate=0 processor.max_cstate=0 idle=mwait intel_pstate=disable\"" /etc/default/grub
