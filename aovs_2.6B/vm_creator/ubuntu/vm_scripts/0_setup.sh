5fbcefe07269c051382880d88d4412f9ef0f3604
root <root@saturn.netronome.com>
2017-09-11 08:06:22 +0200

    Exclude iperf3, netperf from default VM backing image

--- aovs_2.6B/vm_creator/ubuntu/vm_scripts/0_setup.sh
+++ aovs_2.6B/vm_creator/ubuntu/vm_scripts/0_setup.sh
@@ -4,8 +4,8 @@
 /root/vm_scripts/3_install_build_dpdk.sh
 /root/vm_scripts/4_build_l2fwd.sh
 /root/vm_scripts/5_build_pktgen.sh
-/root/vm_scripts/6_install_iperf3.sh
-/root/vm_scripts/7_install_netperf.sh
+#/root/vm_scripts/6_install_iperf3.sh
+#/root/vm_scripts/7_install_netperf.sh
 #/root/vm_scripts/8_build_moongen.sh
 
 sed -i '/exit 0/d' /etc/rc.local
