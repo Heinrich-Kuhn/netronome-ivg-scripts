182c0ab148118994f36916e22b5d5e9189079ff4
Heinrich Kuhn <heinrich.kuhn@netronome.com>
2017-09-14 11:57:49 +0200

    Add test case 7 XVIO VXLAN

---
+++ aovs_2.6B/test_case_7/2_configure_ovs.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [ -z “$1” ]; then
+   echo "ERROR: Number of XVIO CPU's was not passed to this scipt"
+   echo "Example: ./2_configure_ovs.sh <number_of_xvio_cpu's"
+   exit -1
+   else
+   XVIO_CPU_COUNT=$1
+fi
+
+echo "CURRENT configuration"
+cat /etc/netronome.conf
+
+card_node=$(cat /sys/bus/pci/drivers/nfp/0*/numa_node | head -n1 | cut -d " " -f1)
+nfp_cpu_list=$(lscpu -a -p | awk -F',' -v var="$card_node" '$4 == var {printf "%s%s",sep,$1; sep=" "} END{print ""}')
+xvio_cpus_list=()
+nfp_cpu_list=( $nfp_cpu_list )
+
+for counter in $(seq 0 $((XVIO_CPU_COUNT-1)))
+  do
+	xvio_cpus_list+=( "${nfp_cpu_list[$counter+1]}" )
+done
+
+for counter in $(seq 0 $((XVIO_CPU_COUNT-1)))
+  do
+	nfp_cpu_list=( "${nfp_cpu_list[@]:1}" )
+done
+
+xvio_cpus_string=$(IFS=',';echo "${xvio_cpus_list[*]}";IFS=$' \t\n')
+
+cat > /etc/netronome.conf << EOF
+SDN_VIRTIORELAY_ENABLE=y
+SDN_VIRTIORELAY_PARAM="--cpus=$xvio_cpus_string --enable-tso --enable-mrgbuf --vhost-username=libvirt-qemu --vhost-groupname=kvm --huge-dir=/mnt/huge --ovsdb-sock=/var/run/openvswitch/db.sock"
+SDN_FIREWALL=n
+EOF
+
+echo "NEW configuration"
+cat /etc/netronome.conf
+
+ovs-ctl status
+ovs-ctl stop
+ovs-ctl start
+ovs-ctl status
+
+
---
+++ aovs_2.6B/test_case_7/setup_test_case_7.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ ! -z "$v1" ] && [ -e "$2" ] && [ -e "$3" ]; then
+      echo "ERROR: Not enough arguments where passed"
+      echo "Example: ./6_vm_pinning <vm_name> <number_of_vm_cpu's> <number_of_xvio_cpu's"
+      exit -1
+   else
+      VM_NAME=$1
+      CPU_COUNT=$2
+      XVIO_CPU_COUNT=$3
+fi
+
+script_dir="$(dirname $(readlink -f $0))"
+
+echo $VM_NAME
+echo $VM_CPU_COUNT
+
+.script_dir/1_bind_IGB-UIO_driver.sh
+.script_dir/2_configure_ovs.sh $XVIO_CPU_COUNT
+.script_dir/3_configure_ovs_rules.sh $BONDBR_DEST_IP $BONDBR_SRC_IP
+.script_dir/4_configure_apparmor.sh
+.script_dir/5_configure_guest_xml.sh $VM_NAME
+.script_dir/6_vm_pinning.sh $VM_NAME $VM_CPU_COUNT $XVIO_CPU_COUNT
+
