179d6e32c952111755a13edd9fdf244da0fbf16e
Heinrich Kuhn <heinrich.kuhn@netronome.com>
2017-09-14 12:19:14 +0200

    typo

--- aovs_2.6B/test_case_7/setup_test_case_7.sh
+++ aovs_2.6B/test_case_7/setup_test_case_7.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -z "$1" ] && [ -z "$2" ] && [ -z "$3" ]; then
+if [ -z "$1" ] && [ -z "$2" ] && [ -z "$3" ] && [ -z "$4" ] && [ -z "$5" ]; then
       echo "ERROR: Not enough arguments where passed"
       echo "Example: ./6_vm_pinning <vm_name> <number_of_vm_cpu's> <number_of_xvio_cpu's"
       exit -1
@@ -8,6 +8,9 @@ if [ -z "$1" ] && [ -z "$2" ] && [ -z "$3" ]; then
       VM_NAME=$1
       CPU_COUNT=$2
       XVIO_CPU_COUNT=$3
+      BONDBR_DEST_IP=$4
+      BONDBR_SRC_IP=$5
+
 fi
 
 script_dir="$(dirname $(readlink -f $0))"
