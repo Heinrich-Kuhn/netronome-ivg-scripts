2931c62395fb9e89618b32aec81661177204fb73
Heinrich Kuhn <heinrich.kuhn@netronome.com>
2017-09-14 11:06:20 +0200

    Add test case 6 - SR-IOV VXLAN

---
+++ aovs_2.6B/test_case_6/4_guest_xml_configure.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+VM_NAME=$1
+
+# Remove vhostuser interface
+EDITOR='sed -i "/<interface type=.vhostuser.>/,/<\/interface>/d"' virsh edit $VM_NAME
+EDITOR='sed -i "/<hostdev mode=.subsystem. type=.pci./,/<\/hostdev>/d"' virsh edit $VM_NAME
+
+# Add vhostuser interfaces
+# nfp_v0.10 --> 0000:03:09.2
+# nfp_v0.11 --> 0000:03:09.3
+
+bus=$(ethtool -i nfp_v0.10 | grep bus-info | awk '{print $5}' | awk -F ':' '{print $2}')
+EDITOR='sed -i "/<devices/a \<hostdev mode=\"subsystem\" type=\"pci\" managed=\"yes\">  <source> <address domain=\"0x0000\" bus=\"0x'${bus}'\" slot=\"0x09\" function=\"0x2\"\/> <\/source>  <address type=\"pci\" domain=\"0x0000\" bus=\"0x00\" slot=\"0x06\" function=\"0x0\"\/> <\/hostdev>"' virsh edit $VM_NAME
+
+EDITOR='sed -i "/<devices/a \<hostdev mode=\"subsystem\" type=\"pci\" managed=\"yes\">  <source> <address domain=\"0x0000\" bus=\"0x'${bus}'\" slot=\"0x09\" function=\"0x3\"\/> <\/source>  <address type=\"pci\" domain=\"0x0000\" bus=\"0x00\" slot=\"0x07\" function=\"0x0\"\/> <\/hostdev>"' virsh edit $VM_NAME
+
+sleep 5
+
+virsh start $1
---
+++ aovs_2.6B/test_case_6/5_vm_pinning.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+card_node=$(cat /sys/bus/pci/drivers/nfp/0*/numa_node | head -n1 | cut -d " " -f1)
+nfp_cpu_list=$(lscpu -a -p | awk -F',' -v var="$card_node" '$4 == var {printf "%s%s",sep,$1; sep=" "} END{print ""}')
+
+#Check if VM name is passed
+if [ -z “$1” ]; then
+   echo "ERROR: Please pass a VM name that you whish to pin as the first parameter of this script..."
+   exit -1
+   else
+   VM_NAME=$1
+fi
+
+if [ -z “$2” ]; then
+   echo "Using default 4 CPU's for VM"
+   else
+   CPU_COUNT=$2
+fi
+
+CPU_COUNT=$2
+
+echo $VM_NAME
+
+nfp_cpu_list=($nfp_cpu_list)
+echo "nfp_cpu_list: ${nfp_cpu_list[@]}"
+sleep 5
+
+for counter in $(seq 0 $((CPU_COUNT-1)))
+  do
+    virsh --quiet vcpupin $VM_NAME $counter ${nfp_cpu_list[$counter+1]} --config
+  done
+virsh vcpupin $VM_NAME
+
+exit 0
