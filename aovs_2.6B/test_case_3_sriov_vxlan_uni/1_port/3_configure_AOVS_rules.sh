bd7c7016733bd28f9307d5e01ebcae4b768f2d94
root <root@juno.netronome.com>
2017-09-18 17:46:40 +0200

    fix vxlan bug

--- aovs_2.6B/test_case_3_sriov_vxlan_uni/1_port/3_configure_AOVS_rules.sh
+++ aovs_2.6B/test_case_3_sriov_vxlan_uni/1_port/3_configure_AOVS_rules.sh
@@ -13,6 +13,7 @@ else
 fi    
 
 BRIDGE=br0
+BRIDGE_BOND=bondbr0
 
 # Delete all bridges
 for br in $(ovs-vsctl list-br);
@@ -20,22 +21,30 @@ do
   ovs-vsctl --if-exists del-br $br
 done
 
-# Create a new bridge
 ovs-vsctl add-br $BRIDGE
+ovs-vsctl add-br $BRIDGE_BOND
 
 #Add PF to br0
-ovs-vsctl add-port $BRIDGE nfp_p0 -- set interface nfp_p0 ofport_request=1
+ovs-vsctl add-port $BRIDGE_BOND nfp_p0 -- set interface nfp_p0 ofport_request=1
+ovs-vsctl add-port $BRIDGE_BOND patch-bond-to-br -- set interface patch-bond-to-br type=patch options:peer=patch-br-to-bond
+ovs-vsctl add-port $BRIDGE patch-br-to-bond -- set interface patch-br-to-bond type=patch options:peer=patch-bond-to-br
 
 #Add VF's to br0 
 ovs-vsctl add-port $BRIDGE nfp_v0.10 -- set interface nfp_v0.10 ofport_request=10
 ovs-vsctl add-port $BRIDGE nfp_v0.11 -- set interface nfp_v0.11 ofport_request=11
 
+ovs-vsctl show
+ovs-ofctl show $BRIDGE
+
+ovs-ofctl dump-flows $BRIDGE
+ovs-ofctl dump-flows $BRIDGE_BOND
+
+#Delete patch
+ovs-vsctl del-port patch-bond-to-br
+
 ifconfig $BRIDGE_BOND $BONDBR_SRC_IP
 
 ovs-vsctl add-port br0 vxlan01 -- set interface vxlan01 type=vxlan options:remote_ip=$BONDBR_DEST_IP
-
-ovs-ofctl show $BRIDGE
-ovs-ofctl dump-flows $BRIDGE
 
 
 ovs-vsctl set Open_vSwitch . other_config:max-idle=300000
@@ -43,4 +52,5 @@ ovs-vsctl set Open_vSwitch . other_config:flow-limit=1000000
 ovs-appctl upcall/set-flow-limit 1000000
 
 exit 0
+
 
