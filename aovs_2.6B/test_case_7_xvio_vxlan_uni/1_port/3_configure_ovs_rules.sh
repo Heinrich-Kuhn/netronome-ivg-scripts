35237b9589f0db9145c04730170bb2cd7e140022
Heinrich Kuhn <heinrich.kuhn@netronome.com>
2017-09-18 17:56:47 +0200

    fix vxlan xvio

---
+++ aovs_2.6B/test_case_7_xvio_vxlan_uni/1_port/3_configure_ovs_rules.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+BONDBR_DEST_IP=$1 
+BONDBR_SRC_IP=$2
+
+if [ -z "$1" ] && [ -z "$2" ]; then
+    echo "ERROR: Not enough parameters where passed to this script"
+    echo "Example: ./3_configure_AOVS_rules.sh 10.10.10.1 10.10.10.2"
+    exit -1
+else
+    BONDBR_DEST_IP=$1 
+    BONDBR_SRC_IP=$2
+fi    
+
+BRIDGE=br0
+BRIDGE_BOND=bondbr0
+
+# Delete all bridges
+for br in $(ovs-vsctl list-br);
+do
+  ovs-vsctl --if-exists del-br $br
+done
+
+ovs-vsctl add-br $BRIDGE
+ovs-vsctl add-br $BRIDGE_BOND
+
+#Add PF to br0
+ovs-vsctl add-port $BRIDGE_BOND nfp_p0 -- set interface nfp_p0 ofport_request=1
+ovs-vsctl add-port $BRIDGE_BOND patch-bond-to-br -- set interface patch-bond-to-br type=patch options:peer=patch-br-to-bond
+ovs-vsctl add-port $BRIDGE patch-br-to-bond -- set interface patch-br-to-bond type=patch options:peer=patch-bond-to-br
+
+#Add VF's to br0 
+ovs-vsctl add-port $BRIDGE nfp_v0.10 -- set interface nfp_v0.25 ofport_request=25 external_ids:virtio_relay=25
+ovs-vsctl add-port $BRIDGE nfp_v0.11 -- set interface nfp_v0.26 ofport_request=26 external_ids:virtio_relay=26
+
+ovs-vsctl show
+ovs-ofctl show $BRIDGE
+
+ovs-ofctl dump-flows $BRIDGE
+ovs-ofctl dump-flows $BRIDGE_BOND
+
+#Delete patch
+ovs-vsctl del-port patch-bond-to-br
+
+ifconfig $BRIDGE_BOND $BONDBR_SRC_IP
+
+ovs-vsctl add-port br0 vxlan01 -- set interface vxlan01 type=vxlan options:remote_ip=$BONDBR_DEST_IP
+
+
+ovs-vsctl set Open_vSwitch . other_config:max-idle=300000
+ovs-vsctl set Open_vSwitch . other_config:flow-limit=1000000
+ovs-appctl upcall/set-flow-limit 1000000
+
+exit 0
+
+
---
+++ aovs_2.6B/test_case_7_xvio_vxlan_uni/1_port/setup_test_case_7.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ -z "$1" ] && [ -z "$2" ] && [ -z "$3" ] && [ -z "$4" ] && [ -z "$5" ]; then
+      echo "ERROR: Not enough arguments where passed"
+      echo "Example: ./6_vm_pinning <vm_name> <number_of_vm_cpu's> <number_of_xvio_cpu's <local_bridge_ip> <remote_bridge_ip>"
+      exit -1
+   else
+      VM_NAME=$1
+      VM_CPU_COUNT=$2
+      XVIO_CPU_COUNT=$3
+      BONDBR_DEST_IP=$4
+      BONDBR_SRC_IP=$5
+
+fi
+
+script_dir="$(dirname $(readlink -f $0))"
+
+IVG_dir="$(echo $script_dir | sed 's/\(IVG\).*/\1/g')"
+$IVG_dir/helper_scripts/vm_shutdown.sh
+
+echo $VM_NAME
+echo $VM_CPU_COUNT
+
+$script_dir/0_configure_hugepages.sh
+$script_dir/1_bind_IGB-UIO_driver.sh
+$script_dir/2_configure_ovs.sh $XVIO_CPU_COUNT
+$script_dir/3_configure_ovs_rules.sh $BONDBR_DEST_IP $BONDBR_SRC_IP
+$script_dir/4_configure_apparmor.sh
+$script_dir/5_configure_guest_xml.sh $VM_NAME
+$script_dir/6_vm_pinning.sh $VM_NAME $VM_CPU_COUNT $XVIO_CPU_COUNT
+
