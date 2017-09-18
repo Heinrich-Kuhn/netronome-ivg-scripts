02b240ab0aea3649c151fe5b6dd4c23e174ef287
Heinrich Kuhn <heinrich.kuhn@netronome.com>
2017-09-18 15:04:52 +0200

    Add python graph python script

--- aovs_2.6B/vm_creator/ubuntu/vm_scripts/1_install_prerequisites.sh
+++ aovs_2.6B/vm_creator/ubuntu/vm_scripts/1_install_prerequisites.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get -y install make cmake gcc libpcap-dev python unzip python-scapy
+apt-get -y install make cmake gcc libpcap-dev python unzip python-scapy python-pip
+
+pip install numpy
+pip install plotly
