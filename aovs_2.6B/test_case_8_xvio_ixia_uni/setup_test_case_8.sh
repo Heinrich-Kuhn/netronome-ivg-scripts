2c6c742e829d7910c550ecea10ab31f18b476e5c
Heinrich Kuhn <heinrich.kuhn@netronome.com>
2017-09-18 09:53:55 +0200

    Test case 8 typo

--- aovs_2.6B/test_case_8_xvio_ixia_uni/setup_test_case_8.sh
+++ aovs_2.6B/test_case_8_xvio_ixia_uni/setup_test_case_8.sh
@@ -15,10 +15,10 @@ script_dir="$(dirname $(readlink -f $0))"
 echo $VM_NAME
 echo $VM_CPU_COUNT
 
-$script_dir/test_case_3/0_configure_hugepages.sh
-$script_dir/test_case_3/1_bind_IGB-UIO_driver.sh
-$script_dir/test_case_3/2_configure_ovs.sh $XVIO_CPU_COUNT
-$script_dir/test_case_3/3_configure_ovs_rules.sh
-$script_dir/test_case_3/4_configure_apparmor.sh
-$script_dir/test_case_3/5_configure_guest_xml.sh $VM_NAME
-$script_dir/test_case_3/6_vm_pinning.sh $VM_NAME $VM_CPU_COUNT $XVIO_CPU_COUNT
+$script_dir/0_configure_hugepages.sh
+$script_dir/1_bind_IGB-UIO_driver.sh
+$script_dir/2_configure_ovs.sh $XVIO_CPU_COUNT
+$script_dir/3_configure_ovs_rules.sh
+$script_dir/4_configure_apparmor.sh
+$script_dir/5_configure_guest_xml.sh $VM_NAME
+$script_dir/6_vm_pinning.sh $VM_NAME $VM_CPU_COUNT $XVIO_CPU_COUNT
