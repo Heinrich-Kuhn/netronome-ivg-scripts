ba6c29ba6fe02852501c0e7378185161686f31bd
Heinrich Kuhn <heinrich.kuhn@netronome.com>
2017-09-20 10:40:49 +0200

    Fix cloud download install

--- aovs_2.6B/vm_creator/ubuntu/x_create_backing_image.sh
+++ aovs_2.6B/vm_creator/ubuntu/x_create_backing_image.sh
@@ -4,7 +4,7 @@ script_dir="$(dirname $(readlink -f $0))"
 IVG_dir="$(echo $script_dir | sed 's/\(IVG\).*/\1/g')"
 $IVG_dir/helper_scripts/y_vm_shutdown.sh
 
-./0_download_cloud_image.sh
+$script_dir/0_download_cloud_image.sh
 if [ $? == 1 ]
 then exit 1
 fi
