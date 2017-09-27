d5a46fd328ebae54911f23470c3a77d0320cf44b
root <root@h.maas>
2017-09-27 04:54:35 +0000

    update

---
+++ aovs_2.6B/test_case_11_kovs_vxlan_uni_intel/setup_test_case_11.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+VM_NAME=$1
+INTERFACE1=$2
+INTERFACE2=$3
+BONDBR_DEST_IP=$4
+BONDBR_SRC_IP=$5
+
+script_dir="$(dirname $(readlink -f $0))"
+
+$script_dir/0_uninstall_aovs.sh
+$script_dir/1_install_prerequisitions.sh
+$script_dir/2_install_kovs.sh
+$script_dir/3_install_corenic.sh
+
+if [ $? == 1 ]; then 
+echo "Could not install CoreNIC"
+exit -1
+fi
+
+$script_dir/4_configure_bridge.sh $BONDBR_DEST_IP $BONDBR_SRC_IP
+$script_dir/5_add_bridge_interface.sh $VM_NAME $INTERFACE1
+$script_dir/5_add_bridge_interface.sh $VM_NAME $INTERFACE2
+$script_dir/6_fix_apparmor.sh
+
+exit 0
+
