ab613dc50623352104fa7535cdaded4f22dc59e6
Heinrich Kuhn <heinrich.kuhn@netronome.com>
2017-10-04 10:36:34 +0200

    Add ubuntu support ping script

--- aovs_2.6B/test_case_1_ping/1_bind_netronome_nfp_netvf_driver.sh
+++ aovs_2.6B/test_case_1_ping/1_bind_netronome_nfp_netvf_driver.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+ovs-ctl start
+
 PCIA="$(ethtool -i nfp_v0.1 | grep bus | cut -d ' ' -f 5)"
 
 # Bind VF using nfp
 IP=$1
+
+grep ID_LIKE /etc/os-release | grep -q debian
+if [[ $? -eq 0 ]]; then
 driver=nfp
+fi
+
+grep  ID_LIKE /etc/os-release | grep -q fedora
+if [[ $? -eq 0 ]]; then
+driver=nfp_netvf
+fi
 
 DPDK_DEVBIND=$(find /opt/netronome -iname dpdk-devbind.py | head -1)
 if [ "$DPDK_DEVBIND" == "" ]; then
