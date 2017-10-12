8447e61ade7b228009f0cca1569eda0a721b786c
Heinrich Kuhn <heinrich.kuhn@netronome.com>
2017-10-12 09:47:08 +0200

    modify VM xml edit

--- aovs_2.6B/test_case_2_sriov_uni/1_port/4_guest_xml_configure.sh
+++ aovs_2.6B/test_case_2_sriov_uni/1_port/4_guest_xml_configure.sh
@@ -9,6 +9,8 @@ if [ -z "$1" ]; then
    VM_NAME=$1
 fi
 
+s_bus=$(ethtool -i nfp_v0.42 | grep bus-info | awk '{print $5}' | awk -F ':' '{print $2}')
+
 # Remove vhostuser interface
 EDITOR='sed -i "/<interface type=.vhostuser.>/,/<\/interface>/d"' virsh edit $VM_NAME
 EDITOR='sed -i "/<hostdev mode=.subsystem. type=.pci./,/<\/hostdev>/d"' virsh edit $VM_NAME
@@ -17,9 +19,29 @@ EDITOR='sed -i "/<hostdev mode=.subsystem. type=.pci./,/<\/hostdev>/d"' virsh ed
 # nfp_v0.41 --> 0000:81:0d.1
 # nfp_v0.42 --> 0000:81:0d.2
 
-bus=$(ethtool -i nfp_v0.42 | grep bus-info | awk '{print $5}' | awk -F ':' '{print $2}')
-EDITOR='sed -i "/<devices/a \<hostdev mode=\"subsystem\" type=\"pci\" managed=\"yes\">  <source> <address domain=\"0x0000\" bus=\"0x'${bus}'\" slot=\"0x0d\" function=\"0x1\"\/> <\/source>  <address type=\"pci\" domain=\"0x0000\" bus=\"0x00\" slot=\"0x0a\" function=\"0x0\"\/> <\/hostdev>"' virsh edit $VM_NAME
+cat > /tmp/interface << EOL
+      <interface type='hostdev' managed='yes'>
+          <source>
+              <address type='pci' domain='0x0000' bus='0x${s_bus}' slot='0x0d' function='0x01'/>
+          </source>
+          <address type='pci' domain='0x0000' bus='0x00' slot='0x0a' function='0x00'/>
+      </interface>
+EOL
+  virsh attach-device $VM_NAME /tmp/interface --config
 
-EDITOR='sed -i "/<devices/a \<hostdev mode=\"subsystem\" type=\"pci\" managed=\"yes\">  <source> <address domain=\"0x0000\" bus=\"0x'${bus}'\" slot=\"0x0d\" function=\"0x2\"\/> <\/source>  <address type=\"pci\" domain=\"0x0000\" bus=\"0x00\" slot=\"0x0b\" function=\"0x0\"\/> <\/hostdev>"' virsh edit $VM_NAME
+sleep 1
+echo "Device attached"
+
+cat > /tmp/interface << EOL
+      <interface type='hostdev' managed='yes'>
+          <source>
+              <address type='pci' domain='0x0000' bus='0x${s_bus}' slot='0x0d' function='0x02'/>
+          </source>
+          <address type='pci' domain='0x0000' bus='0x00' slot='0x0b' function='0x00'/>
+      </interface>
+EOL
+  virsh attach-device $VM_NAME /tmp/interface --config
+
+echo "Device attached"
 
 exit 0
