d3326b9b2d8f23467187fa463ddf20ac57dd5812
netronome-support <>
2017-10-17 11:07:33 +0200

    Configure XML scripts changed to optimise performance

--- aovs_2.6B/test_case_3_sriov_vxlan_uni/1_port/4_guest_xml_configure.sh
+++ aovs_2.6B/test_case_3_sriov_vxlan_uni/1_port/4_guest_xml_configure.sh
@@ -21,4 +21,21 @@ EDITOR='sed -i "/<devices/a \<hostdev mode=\"subsystem\" type=\"pci\" managed=\"
 
 EDITOR='sed -i "/<devices/a \<hostdev mode=\"subsystem\" type=\"pci\" managed=\"yes\">  <source> <address domain=\"0x0000\" bus=\"0x'${bus}'\" slot=\"0x09\" function=\"0x3\"\/> <\/source>  <address type=\"pci\" domain=\"0x0000\" bus=\"0x00\" slot=\"0x0b\" function=\"0x0\"\/> <\/hostdev>"' virsh edit $VM_NAME
 
+
+# Configuring default settings for VM
+VM_CPU=$(virsh dominfo $VM_NAME | grep 'CPU(s):' | cut -d ':' -f2 | cut -d ' ' -f10)
+max_memory=$(virsh dominfo $VM_NAME | grep 'Max memory:' | awk '{print $3}')
+EDITOR='sed -i "/<numa>/,/<\/numa>/d"' virsh edit $VM_NAME
+EDITOR='sed -i "/vcpu/d"' virsh edit $VM_NAME
+EDITOR='sed -i "/<cpu/,/<\/cpu>/d"' virsh edit $VM_NAME
+EDITOR='sed -i "/<memoryBacking>/,/<\/memoryBacking>/d"' virsh edit $VM_NAME
+virsh setvcpus $VM_NAME $VM_CPU --config --maximum
+virsh setvcpus $VM_NAME $VM_CPU --config
+# MemoryBacking
+EDITOR='sed -i "/<domain/a \<memoryBacking><hugepages><page size=\"2048\" unit=\"KiB\" nodeset=\"0\"\/><\/hugepages><\/memoryBacking>"' virsh edit $VM_NAME
+# CPU
+echo VM_CPU: $VM_CPU
+EDITOR='sed -i "/<domain/a \<cpu mode=\"host-model\"><model fallback=\"allow\"\/><numa><cell id=\"0\" cpus=\"0-'$((VM_CPU-1))'\" memory=\"'${max_memory}'\" unit=\"KiB\" memAccess=\"shared\"\/><\/numa><\/cpu>"' virsh edit $VM_NAME
+
+
 sleep 5
