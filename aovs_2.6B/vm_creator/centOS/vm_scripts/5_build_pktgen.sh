671a2bae16b0f84af14a330938e6d1a641f6a00f
root <root@neo.netronome.com>
2017-10-31 17:41:11 +0000

    Fix l2fwd

--- aovs_2.6B/vm_creator/centOS/vm_scripts/5_build_pktgen.sh
+++ aovs_2.6B/vm_creator/centOS/vm_scripts/5_build_pktgen.sh
@@ -3,29 +3,13 @@
 export DPDK_BASE_DIR=/root
 export DPDK_VERSION=dpdk-stable-16.11.3
 export DPDK_TARGET=x86_64-native-linuxapp-gcc
-export DPDK_BUILD=$DPDK_BASE_DIR/$DPDK_VERSION/$DPDK_TARGET
-export PKTGEN=pktgen-3.4.2
 
-echo "Cleaning.."
-if [ -d "$DPDK_BASE_DIR/$PKTGEN" ]; then
-  rm -rf $DPDK_BASE_DIR/$PKTGEN
-fi
+cd $DPDK_BASE_DIR/$DPDK_VERSION/examples/l2fwd
+sed 's/#define RTE_TEST_RX_DESC_DEFAULT 128/#define RTE_TEST_RX_DESC_DEFAULT 1024/' -i main.c
+sed 's/#define RTE_TEST_TX_DESC_DEFAULT 512/#define RTE_TEST_TX_DESC_DEFAULT 1024/' -i main.c
+sed -E 's/(#define NB_MBUF   )8192/\116384/g' -i main.c
 
-if [ ! -e "$DPDK_BASE_DIR/$PKTGEN.tar.gz" ]; then
-  echo "Downloading.."
-  wget http://dpdk.org/browse/apps/pktgen-dpdk/snapshot/$PKTGEN.tar.gz --directory-prefix=$DPDK_BASE_DIR
-fi
+make RTE_SDK=$DPDK_BASE_DIR/$DPDK_VERSION 
 
-echo "Extracting.."
-tar xf $DPDK_BASE_DIR/$PKTGEN.tar.gz -C $DPDK_BASE_DIR
-cd $DPDK_BASE_DIR/$PKTGEN
-sed 's/DEFAULT_PKT_BURST       = 32/DEFAULT_PKT_BURST       = 64/g' -i ./app/pktgen-constants.h
-sed '/DEFAULT_RX_DESC         =/d' -i ./app/pktgen-constants.h
-sed '/DEFAULT_TX_DESC         =/d' -i ./app/pktgen-constants.h
-sed -i "/DEFAULT_PKT_BURST/aDEFAULT_RX_DESC = 512," ./app/pktgen-constants.h
-sed -i "/DEFAULT_PKT_BURST/aDEFAULT_TX_DESC = 1024," ./app/pktgen-constants.h
-
-make RTE_SDK=$DPDK_BASE_DIR/$DPDK_VERSION RTE_TARGET=$DPDK_TARGET
-
-rm -f /root/dpdk-pktgen
-ln -s $DPDK_BASE_DIR/$PKTGEN/app/app/x86_64-native-linuxapp-gcc/pktgen /root/dpdk-pktgen
+rm -f $DPDK_BASE_DIR/dpdk-l2fwd
+ln -s $DPDK_BASE_DIR/$DPDK_VERSION/examples/l2fwd/build/app/l2fwd $DPDK_BASE_DIR/dpdk-l2fwd
