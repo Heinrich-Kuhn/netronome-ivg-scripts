25270ca928b389bf7eef431fa4ce69e2e1628c54
Tobias Karlsson <Support@netronome.com>
2017-12-18 13:51:05 -0500

    Improvements to system-inventory.sh script. Adding script to install NFP driver.

--- helper_scripts/system-inventory.sh
+++ helper_scripts/system-inventory.sh
@@ -7,7 +7,7 @@ capdir="$tmpdir/$capname"
 
 mkdir -p $capdir
 
-###################################################
+########################################################
 # Copy system files
 
 list=()
@@ -45,14 +45,23 @@ list+=( "/sys/module/nfp_offloads/control/rh_entries" )
 list+=( "/sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages" )
 
 ########################################################
-cplist=()
+if [ -d /sys/bus/pci/drivers/nfp ]; then
+    nfplist=$(find /sys/bus/pci/drivers/nfp -type l -name '*:*:*.*')
+    for nfpdir in $nfplist ; do
+        list+=( "$nfpdir/numa_node" )
+        list+=( "$nfpdir/irq" )
+    done
+fi
+
+########################################################
 for fname in ${list[@]} ; do
     if [ -e $fname ]; then
-        cplist+=( $fname )
+        /bin/cp -R --parents $fname \
+            --target-directory $capdir
+    else
+        echo "Missing $fname" >> $capdir/missing-files.txt
     fi
 done
-
-/bin/cp -R --parents --target-directory $capdir ${cplist[@]}
 
 ########################################################
 function run () {
@@ -83,9 +92,12 @@ run "yum" "list"                "yum-list.txt"
 run "dpkg" "--get-selections"   "dpkg-pkg-list.txt"
 run "ifconfig" ""               "ifconfig.txt"
 run "arp" "-n"                  "arp-n.txt"
+run "route" "-n"                "route-n.txt"
+run "netstat" "-s"              "netstat-s.txt"
 run "lsmod" ""                  "lsmod.txt"
 run "ps" "aux"                  "ps-aux.txt"
 run "dmidecode" "--type system" "dmidecode.txt"
+run "lshw" ""                   "lshw.txt"
 run "printenv" ""               "printenv.txt"
 
 run "virsh" "--version"         "virsh-version.txt"
@@ -98,6 +110,10 @@ run "/opt/netronome/bin/ovs-ctl" "version" "ovs-version.txt"
 run "/opt/netronome/bin/ovs-ctl" "status" "ovs-status.txt"
 run "/opt/netronome/bin/nfp-hwinfo" "" "nfp-hwinfo.txt"
 run "/opt/netronome/bin/nfp-media" "" "nfp-media.txt"
+run "/opt/netronome/bin/nfp-programmables" "" "nfp-programmables.txt"
+run "/opt/netronome/bin/nfp-arm" "-D" "nfp-arm-D.txt"
+run "/opt/netronome/bin/nfp-phymod" "" "nfp-phymod.txt"
+run "/opt/netronome/bin/nfp-res" "-L" "nfp-res-locks.txt"
 
 run "ovs-dpctl" "dump-flows -m" "ovs-dpctl-flows.txt"
 run "ovs-ctl" "status troubleshoot -C" "ovs-ctl-status-troubleshoot.txt"
@@ -107,7 +123,7 @@ run "nfp" "-m mac show port info 0 4" "nfp-mac-0-0-first.txt"
 run "nfp" "-m mac show port info 0 0" "nfp-mac-0-0-second.txt"
 run "nfp" "-m mac show port info 0 4" "nfp-mac-0-0-second.txt"
 
-run "/opt/netronome/bin/virtio_relay_stats" "" "nfp-virtio-stats.txt"
+run "ovs-appctl" "bond/list" "bond-list.txt"
 
 run "ovs-vsctl" "show" "ovs-vsctl-show.txt"
 
@@ -133,17 +149,23 @@ for pid in $(pgrep virtiorelayd) ; do
 done
 
 ########################################################
+tool="/opt/netronome/bin/virtio_relay_stats"
+if [ -x $tool ] && [ "$(pgrep virtiorelayd)" != "" ]; then
+    $tool > $capdir/nfp-virtio-stats.txt
+fi
+
+########################################################
 virsh="$(which virsh)"
 if [ "$virsh" != "" ]; then
     for vmname in $(virsh list --all --name) ; do
-        vmdir="$capdir/vms/$vmname"
+        vmdir="$capdir/virsh/vms/$vmname"
         mkdir -p $vmdir
         virsh dumpxml $vmname > $vmdir/config.xml
         virsh dominfo $vmname > $vmdir/dominfo.txt
         virsh vcpuinfo $vmname > $vmdir/vcpuinfo.txt
     done
     for netname in $(virsh net-list --name) ; do
-        netdir="$capdir/net/$netname"
+        netdir="$capdir/virsh/net/$netname"
         mkdir -p $netdir
         virsh net-dumpxml $netname > $netdir/config.xml
         virsh net-dhcp-leases $netname > $netdir/leases.txt
@@ -172,11 +194,28 @@ if [ -x /sbin/ethtool ]; then
     iflist=$(cat /proc/net/dev \
         | sed -rn 's/^\s*(\S+):.*$/\1/p')
     ifdir="$capdir/ethtool"
-    mkdir -p $ifdir
+    mkdir -p $ifdir/info $ifdir/features $ifdir/stats
     for ifname in $iflist ; do
-        ethtool -i $ifname > $ifdir/info-$ifname.txt 2>&1
-        ethtool -k $ifname > $ifdir/features-$ifname.txt 2>&1
-        ethtool -S $ifname > $ifdir/stats-$ifname.txt 2>&1
+        ethtool -i $ifname > $ifdir/info/$ifname.txt 2>&1
+        ethtool -k $ifname > $ifdir/features/$ifname.txt 2>&1
+        ethtool -S $ifname > $ifdir/stats/$ifname.txt 2>&1
+    done
+fi
+
+########################################################
+# Capture Link-Aggregation (bonding) Status
+
+listfile="$capdir/bond-list.txt"
+if [ -f $listfile ]; then
+    mkdir -p $capdir/bond
+    bondlist=$(cat $listfile \
+        | tail -n +2 \
+        | cut -f 1)
+    for bondname in $bondlist ; do
+        ovs-appctl bond/show $bondname 2>&1 \
+            > $capdir/bond/$bondname-bond.txt
+        ovs-appctl lacp/show $bondname 2>&1 \
+            > $capdir/bond/$bondname-lacp.txt
     done
 fi
 
@@ -199,10 +238,17 @@ if [ -x $(which ovs-vsctl) ]; then
 fi
         
 ########################################################
+# Copy capture script
+
+/bin/cp $0 --target-directory $capdir
+
+########################################################
 
 tar cz -C $tmpdir -f $HOME/$capname.tgz $capname
 
 /bin/rm -rf $tmpdir
+
+echo "System Inventory Capture file: $HOME/$capname.tgz"
 
 ########################################################
 exit 0
