dcd72ac1b4fd498c04c856097d0c8b6455aa32d8
root <jacobusgericke@netronome.com>
2018-01-19 14:45:36 +0000

    Netplan Update Change

--- aovs_2.6B/vm_creator/ubuntu/vm_scripts/0_setup.sh
+++ aovs_2.6B/vm_creator/ubuntu/vm_scripts/0_setup.sh
@@ -7,15 +7,19 @@
 #/root/vm_scripts/6_install_iperf3.sh
 #/root/vm_scripts/7_install_netperf.sh
 #/root/vm_scripts/8_build_moongen.sh
-/root/vm_scripts/11_install_nfp_drv_kmods.sh || exit -1
+#/root/vm_scripts/11_install_nfp_drv_kmods.sh || exit -1
 
 sed -i '/exit 0/d' /etc/rc.local
 echo "/vm_scripts/samples/2_auto_bind_igb_uio.sh || exit 1" >> /etc/rc.local
 echo "exit 0" >> /etc/rc.local
 
+
+echo "Updating Netplan ..."
 sed -i 's/.*set.*//g' /etc/netplan/50-cloud-init.yaml
 sed -i 's/.*match.*//g' /etc/netplan/50-cloud-init.yaml
 sed -i 's/.*mac.*//g' /etc/netplan/50-cloud-init.yaml
+
+echo "Netplan apply..."
 
 netplan apply
 
