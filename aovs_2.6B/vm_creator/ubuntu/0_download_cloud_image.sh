c55101ba354787b1e54f07fe641c49df64096337
netronome-support <>
2018-02-09 16:44:35 +0200

    Updated download image

--- aovs_2.6B/vm_creator/ubuntu/0_download_cloud_image.sh
+++ aovs_2.6B/vm_creator/ubuntu/0_download_cloud_image.sh
@@ -12,25 +12,36 @@ UBUNTU_CLOUD_IMAGE=ubuntu-17.10-server-cloudimg-amd64.img
 UBUNTU_CLOUD_IMAGE_URL=https://cloud-images.ubuntu.com/releases/17.10/release/$UBUNTU_CLOUD_IMAGE
 cd /var/lib/libvirt/images/
 
-if [ -f $UBUNTU_CLOUD_IMAGE.copy ]; then
-	echo -e "${GREEN}Using ubuntu cloud-image .copy as base image${NC}" 
-	read -p "Do you want to remove previous backing image, this will render VM's using this backing image useless? (y/n): " ans1
-        if [ $ans1 == "y" ]
-        then
-            rm -f $UBUNTU_CLOUD_IMAGE
-            cp $UBUNTU_CLOUD_IMAGE.copy $UBUNTU_CLOUD_IMAGE
-        else
-            exit 1
-        fi
+if [[ -z "$1" ]]; then
+    if [ -f $UBUNTU_CLOUD_IMAGE.copy ]; then
+    	echo -e "${GREEN}Using ubuntu cloud-image .copy as base image${NC}" 
+    	read -p "Do you want to remove previous backing image, this will render VM's using this backing image useless? (y/n): " ans1
+            if [ $ans1 == "y" ]
+            then
+                rm -f $UBUNTU_CLOUD_IMAGE
+                cp $UBUNTU_CLOUD_IMAGE.copy $UBUNTU_CLOUD_IMAGE
+            else
+                exit 1
+            fi
+    else
+    	cd /var/lib/libvirt/images/
+    	echo -e "${GREEN}Downloading ubuntu-17.10-server-cloudimg-amd64.img to /var/lib/libvirt/images/${NC}"
+    	wget --timestamping $UBUNTU_CLOUD_IMAGE_URL
+    	echo -e "${GREEN}Creating copy of ubuntu-cloud-image${NC}"
+    	cp ubuntu-17.10-server-cloudimg-amd64.img ubuntu-17.10-server-cloudimg-amd64.img.copy
+    fi
 else
-	cd /var/lib/libvirt/images/
-	echo -e "${GREEN}Downloading ubuntu-17.10-server-cloudimg-amd64.img to /var/lib/libvirt/images/${NC}"
-	wget --timestamping $UBUNTU_CLOUD_IMAGE_URL
-	echo -e "${GREEN}Creating copy of ubuntu-cloud-image${NC}"
-	cp ubuntu-17.10-server-cloudimg-amd64.img ubuntu-17.10-server-cloudimg-amd64.img.copy
+
+    rm -f $UBUNTU_CLOUD_IMAGE
+    cp $UBUNTU_CLOUD_IMAGE.copy $UBUNTU_CLOUD_IMAGE
+
+    cd /var/lib/libvirt/images/
+    echo -e "${GREEN}Downloading ubuntu-17.10-server-cloudimg-amd64.img to /var/lib/libvirt/images/${NC}"
+    wget --timestamping $UBUNTU_CLOUD_IMAGE_URL
+    echo -e "${GREEN}Creating copy of ubuntu-cloud-image${NC}"
+    cp ubuntu-17.10-server-cloudimg-amd64.img ubuntu-17.10-server-cloudimg-amd64.img.copy
+
 fi
-
-
 
 
 
