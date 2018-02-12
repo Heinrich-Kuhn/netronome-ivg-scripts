3d059cd91bdfdde6bbe6f5f2fc40b521f123bb26
netronome-support <>
2018-02-12 17:27:01 +0200

    typo

--- aovs_2.6B/test_case_6_xvio_uni/1_port/1_bind_IGB-UIO_driver_TC.sh
+++ aovs_2.6B/test_case_6_xvio_uni/1_port/1_bind_IGB-UIO_driver_TC.sh
@@ -57,7 +57,7 @@ echo "VF2_PCI_ADDRESS: $VF2_PCI_ADDRESS"
 
 #------------------------------------------------------------------------------------------------------
 
-sed -e -i "s#^VIRTIOFWD_STATIC_VFS=.*#VIRTIOFWD_STATIC_VFS=($VF1_PCI_ADDRESS=21 $VF2_PCI_ADDRESS=22)"
+sed "s#^VIRTIOFWD_STATIC_VFS=.*#VIRTIOFWD_STATIC_VFS=($VF1_PCI_ADDRESS=1 $VF2_PCI_ADDRESS=2)" -i /etc/default/virtio-forarder
 
 ## BIND IGB_UIO DRIVER
 interface_list=(${VF1_PCI_ADDRESS} ${VF2_PCI_ADDRESS})
