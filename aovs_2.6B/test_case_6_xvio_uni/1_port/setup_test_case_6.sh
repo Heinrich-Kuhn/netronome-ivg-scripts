db8f26aa3e538ca82b3fd99f687ab7b25ace2397
netronome-support <>
2018-02-13 17:09:43 +0200

    Stop virtio before setup

--- aovs_2.6B/test_case_6_xvio_uni/1_port/setup_test_case_6.sh
+++ aovs_2.6B/test_case_6_xvio_uni/1_port/setup_test_case_6.sh
@@ -15,6 +15,8 @@ script_dir="$(dirname $(readlink -f $0))"
 echo $VM_NAME
 echo $VM_CPU_COUNT
 
+
+
 if [[ "$4" == "AOVS" ]]; then
       $script_dir/0_configure_hugepages.sh
       $script_dir/1_bind_IGB-UIO_driver.sh
@@ -24,6 +26,10 @@ if [[ "$4" == "AOVS" ]]; then
       $script_dir/5_configure_guest_xml.sh $VM_NAME
       $script_dir/6_vm_pinning.sh $VM_NAME $VM_CPU_COUNT $XVIO_CPU_COUNT
 elif [[ "$4" == "OVS_TC" ]]; then
+
+      echo "Stoppping virtio-forwarder"
+      systemctl stop virtio-forwarder
+      sleep 10
       $script_dir/0_configure_hugepages.sh
       $script_dir/1_bind_IGB-UIO_driver_TC.sh
       $script_dir/2_configure_ovs_TC.sh $XVIO_CPU_COUNT
@@ -31,10 +37,11 @@ elif [[ "$4" == "OVS_TC" ]]; then
       $script_dir/4_configure_apparmor.sh
       $script_dir/5_configure_guest_xml_TC.sh $VM_NAME
       $script_dir/6_vm_pinning.sh $VM_NAME $VM_CPU_COUNT $XVIO_CPU_COUNT
-      echo "Resetting Vitioforwarder ..."
+      echo "Restarting Vitioforwarder ..."
       sleep 2
-      systemctl restart virtio-forwarder
+      systemctl start virtio-forwarder
       sleep 10
+
 elif [[ "$4" == "CLEAN" ]]; then
       $script_dir/clean.sh $VM_NAME
       echo "Stopping Vitioforwarder ..."
