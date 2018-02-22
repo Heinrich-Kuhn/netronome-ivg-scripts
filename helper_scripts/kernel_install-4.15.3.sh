357c51fcb32bb720c33d4399b74ce9e70ff09df8
netronome-support <>
2018-02-22 13:21:33 +0200

    Update upgrade kernel

---
+++ helper_scripts/kernel_install-4.15.3.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+script_dir="$(dirname $(readlink -f $0))"
+
+grep ID_LIKE /etc/os-release | grep -q debian
+if [[ $? -eq 0 ]]; then
+    #wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.14.13/linux-headers-4.14.13-041413_4.14.13-041413.201801101001_all.deb
+    #wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.14.13/linux-headers-4.14.13-041413-generic_4.14.13-041413.201801101001_amd64.deb
+    #wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.14.13/linux-image-4.14.13-041413-generic_4.14.13-041413.201801101001_amd64.deb
+    wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.15.3/linux-image-4.15.3-041503-generic_4.15.3-041503.201802120730_amd64.deb
+    wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.15.3/linux-headers-4.15.3-041503-generic_4.15.3-041503.201802120730_amd64.deb
+    wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.15.3/linux-headers-4.15.3-041503_4.15.3-041503.201802120730_all.deb
+
+    dpkg -i linux-image-4.15.3-041503-generic_4.15.3-041503.201802120730_amd64.deb
+    dpkg -i linux-headers-4.15.3-041503-generic_4.15.3-041503.201802120730_amd64.deb
+    dpkg -i linux-headers-4.15.3-041503_4.15.3-041503.201802120730_all.deb
+
+    apt-get -f install
+
+    $script_dir/configure_grub.sh
+
+    update-grub2
+fi
+
+
+grep  ID_LIKE /etc/os-release | grep -q fedora
+if [[ $? -eq 0 ]]; then
+
+    rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+    rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm 
+
+    yum --disablerepo="*" --enablerepo="elrepo-kernel" list available
+
+    yum --enablerepo=elrepo-kernel -y install kernel-ml
+
+    $script_dir/configure_grub.sh
+
+    grub2-mkconfig -o /boot/grub2/grub.cfg
+
+fi
+
+
+echo "PLEASE REBOOT MACHINE"
+
+sleep 5
