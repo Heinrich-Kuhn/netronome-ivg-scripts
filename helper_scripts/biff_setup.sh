75b5673ccc77e5f04b30dde4fd34e862e2429b05
netronome-support <>
2018-03-15 13:24:59 +0200

    Update BIFF Setup

--- helper_scripts/biff_setup.sh
+++ helper_scripts/biff_setup.sh
@@ -25,23 +25,23 @@ RX_DUT_IP=$1
 script_dir="$(dirname $(readlink -f $0))"
 IVG_dir="$(echo $script_dir | sed 's/\(IVG\).*/\1/g')"
 
-BIFF_dir=$(find /root/IVG -name "BIFF")
+BIFF_dir=$(find /root -name "BIFF")
 
 # Copy BIFF to DUT
-scp -i ~/.ssh/netronome_key -r $BIFF_dir root@$RX_DUT_IP:/root
-MY_IP=$(ssh -i ~/.ssh/netronome_key root@$RX_DUT_IP who | tail -1 | egrep -o '([0-9]{1,3}\.){3}[0-9]{1,3}')
+scp -i ~/.ssh/netronome_key -r $BIFF_dir root@$RX_DUT_IP:/root/IVG
+#MY_IP=$(ssh -i ~/.ssh/netronome_key root@$RX_DUT_IP who | tail -1 | egrep -o '([0-9]{1,3}\.){3}[0-9]{1,3}')
+MY_IP=$(ip route get $RX_DUT_IP | egrep -o '([0-9]{1,3}\.){3}[0-9]{1,3}' | grep -v $RX_DUT_IP)
 echo "MY IP: $MY_IP"
 
 # Launch Oscar on RX_DUT
-ssh -i ~/.ssh/netronome_key root@$RX_DUT_IP sed -i 's@TargetConnection IP=".*" PORT=".*"@TargetConnection IP=\"'$MY_IP'\" PORT=\"50123\"@g' /root/BIFF/Board-Instrumentation-Framework/Oscar/OscarConfig.xml
+ssh -i ~/.ssh/netronome_key root@$RX_DUT_IP sed -i 's@TargetConnection IP=".*" PORT=".*"@TargetConnection IP=\"'$MY_IP'\" PORT=\"50123\"@g' /root/IVG/BIFF/Board-Instrumentation-Framework/Oscar/OscarConfig.xml
 ssh -i ~/.ssh/netronome_key root@$RX_DUT_IP python3 /root/IVG/BIFF/Board-Instrumentation-Framework/Oscar/Oscar.py &
 
 #Edit Marvin
-sed -i 's@<Network IP=".*" PORT=".*" @<Network IP=\"'$MY_IP'\" PORT=\"50123\"@g'  /root/BIFF/Board-Instrumentation-Framework/Marvin/build/libs/Appliaction.xml
-
+sed -i 's@<Network IP=".*" PORT=".*" @<Network IP=\"'$MY_IP'\" PORT=\"50123\"@g'  /root/BIFF/Board-Instrumentation-Framework/Marvin/build/libs/Application.xml
 #Launch Marvin on Local
 java -jar /root/BIFF/Board-Instrumentation-Framework/Marvin/build/libs/BIFF.Marvin.jar &
 
 # Launch Minions on RX_DUT
 
-ssh -i ~/.ssh/netronome_key root@$RX_DUT_IP python3 Minion.py -i pktgenCapture.xml
+ssh -i ~/.ssh/netronome_key root@$RX_DUT_IP python3 /root/IVG/BIFF/Board-Instrumentation-Framework/Minion/Minion.py -i /root/IVG/BIFF/Board-Instrumentation-Framework/Minion/pktgenCapture.xml
