bca4fd64b13d8b825ff4021e441fd3d77f66e220
netronome-support <>
2018-03-22 15:54:05 +0200

    Changes to DPDK test case

--- aovs_2.6B/test_case_12_dpdk_ovs_uni_intel/setup_test_case_install_12.sh
+++ aovs_2.6B/test_case_12_dpdk_ovs_uni_intel/setup_test_case_install_12.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-
+IVG_dir="/root/IVG_folder"
 script_dir="$(dirname $(readlink -f $0))"
+DPDK_VER=$1
+OVS_VER=$2
 
 $script_dir/0_uninstall_aovs.sh
-$script_dir/1_install_dpdk.sh
-$script_dir/2_install_dpdk_ovs.sh
+#$script_dir/1_install_dpdk.sh
+$IVG_dir/helper_scripts/install-dpdk.sh $DPDK_VER
+$script_dir/2_install_dpdk_ovs.sh $DPDK_VER $OVS_VER
 
 if [ $? == 1 ]; then 
 echo "Could not install DPDK OVS"
 exit -1
 fi
 
-$script_dir/3_install_intel.sh
+$script_dir/3_install_intel.sh 
 
 if [ $? == 1 ]; then 
 echo "Could not install Intel driver"
