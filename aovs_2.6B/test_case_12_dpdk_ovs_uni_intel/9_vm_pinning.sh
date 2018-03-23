e0d77d0d5a13e98867e1370cb3468efbb9b6fb7b
netronome-support <>
2018-03-23 16:16:48 +0200

    Typos

--- aovs_2.6B/test_case_12_dpdk_ovs_uni_intel/9_vm_pinning.sh
+++ aovs_2.6B/test_case_12_dpdk_ovs_uni_intel/9_vm_pinning.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-card_node=$(cat /sys/bus/pci/drivers/nfp/0*/numa_node | head -n1 | cut -d " " -f1)
+card_node=$(cat /sys/bus/pci/drivers/igb/0*/numa_node | head -n1 | cut -d " " -f1)
 nfp_cpu_list=$(lscpu -a -p | awk -F',' -v var="$card_node" '$4 == var {printf "%s%s",sep,$1; sep=" "} END{print ""}')
 
 #Check if VM name is passed
