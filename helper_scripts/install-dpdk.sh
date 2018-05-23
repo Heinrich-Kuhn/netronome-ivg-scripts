edcd53afc74d77054191fd97c4a15ed8e642f820
Tobias Karlsson <Support@netronome.com>
2018-05-23 19:29:32 -0400

    Improved the install-dpdk.sh script.

--- helper_scripts/install-dpdk.sh
+++ helper_scripts/install-dpdk.sh
@@ -16,10 +16,16 @@ fi
 pkgname="dpdk"
 
 # Installation Directory
-srcdir="/opt/src"
+: "${DPDK_INSTALL_DIR:=/opt/src}"
 
 # Compile Target Architecture for DPDK
-export RTE_TARGET="x86_64-native-linuxapp-gcc"
+: "${RTE_TARGET:=x86_64-native-linuxapp-gcc}"
+
+# DPDK Download Directory
+: "${DPDK_DOWNLOAD_DIR:=/var/cache/download}"
+
+# DPDK Web Site
+: "${DPDK_DOWNLOAD_URL:=https://fast.dpdk.org/rel}"
 
 ########################################
 
@@ -33,6 +39,8 @@ function check_status () {
 
 ########################################
 ##  Install pre-requisites (assuming the tool is available)
+
+kvers=$(uname -r)
 
 prereqs=()
 prereqs+=( "wget@" )
@@ -51,6 +59,8 @@ $IVG_dir/helper_scripts/install-packages.sh ${prereqs[@]}
 srchlist=()
 srchlist+=( "$(pwd)" )
 srchlist+=( "$HOME" )
+srchlist+=( "$DPDK_DOWNLOAD_DIR" )
+srchlist+=( "/var/cache/download" )
 srchlist+=( "/opt/download" )
 srchlist+=( "/pkgs/dpdk" )
 srchlist+=( "/tmp" )
@@ -71,16 +81,14 @@ fi
 ########################################
 
 if [ "$pkgfile" == "" ] && [ "$pkgdir" == "" ]; then
-    dldir="/opt/download"
-    url="https://fast.dpdk.org/rel"
     fname="$pkgname-$version.tar.xz"
-    mkdir -p $dldir
-        check_status "failed to create $dldir"
-    dlfile="$dldir/pend-$fname"
-    echo " - Downloading $url/$fname"
-    wget --no-verbose "$url/$fname" -O "$dlfile"
-        check_status "failed to download $url/$fname"
-    pkgfile="$dldir/$fname"
+    mkdir -p $DPDK_DOWNLOAD_DIR
+        check_status "failed to create $DPDK_DOWNLOAD_DIR"
+    dlfile="$DPDK_DOWNLOAD_DIR/pend-$fname"
+    echo " - Downloading $DPDK_DOWNLOAD_URL/$fname"
+    wget --no-verbose "$DPDK_DOWNLOAD_URL/$fname" -O "$dlfile"
+        check_status "failed to download $DPDK_DOWNLOAD_URL/$fname"
+    pkgfile="$DPDK_DOWNLOAD_DIR/$fname"
     /bin/mv -f "$dlfile" "$pkgfile"
         check_status "failed to move $dlfile"
 fi
@@ -101,9 +109,9 @@ fi
 ########################################
 
 if [ "$pkgdir" == "" ]; then
-    mkdir -p $srcdir
+    mkdir -p $DPDK_INSTALL_DIR
 
-    tar x -C $srcdir -f $pkgfile
+    tar x -C $DPDK_INSTALL_DIR -f $pkgfile
         check_status "failed to un-tar $pkgfile"
 
     tardir=$(tar t -f $pkgfile \
@@ -112,7 +120,7 @@ if [ "$pkgdir" == "" ]; then
 
         check_status "failed to determine package directory"
 
-    pkgdir="$srcdir/$tardir"
+    pkgdir="$DPDK_INSTALL_DIR/$tardir"
 fi
 
 export RTE_SDK="$pkgdir"
@@ -141,20 +149,82 @@ make -C $RTE_SDK config $opts
 
 ########################################
 
-sed -r 's/(CONFIG_RTE_LIBRTE_NFP_PMD)=.*$/\1=y/' \
-    -i $RTE_SDK/build/.config
+ss=""
+ss="${ss}"'s/(CONFIG_RTE_LIBRTE_NFP_PMD)=.*$/\1=y/;'
+
+########################################
+# Custom configuration (via DPDK_CUSTOM_CONFIG)
+
+idx=1
+while : ; do
+    config=$(echo "$DPDK_CUSTOM_CONFIG;" | cut -d ';' -f $idx)
+    if [ "$config" == "" ]; then
+        break
+    fi
+    varname=${config/=*/}
+    value=${config/*=/}
+    if [ "$varname" != "" ] && [ "$value" != "" ]; then
+        ss="${ss}s/($varname)=.*\$/\1=$value/;"
+    fi
+    idx=$(( idx + 1 ))
+done
+
+########################################
+sed -r "$ss" -i $RTE_SDK/build/.config \
+    || exit -1
+
+########################################
+# Save a copy of the configuration
+
+buildconfig="$RTE_SDK/build/build.config"
+
+if [ -f $buildconfig ]; then
+    /bin/mv -f $buildconfig $buildconfig.old \
+        || exit -1
+fi
+/bin/cp -f $RTE_SDK/build/.config \
+    ${buildconfig}.pending \
+    || exit -1
 
 ########################################
 
-make -C $RTE_SDK
+make -C $RTE_SDK \
+    | tee $RTE_SDK/build/make.log
 
     check_status "failed to make DPDK"
+
+########################################
+
+make -C $RTE_SDK install
+
+    check_status "failed to install DPDK"
+
+########################################
+# Move the pending build config
+
+/bin/mv ${buildconfig}.pending $buildconfig \
+    || exit -1
+
+########################################
+
+depmod -a
 
 ########################################
 
 # Not sure why this is needed, but I can't build other apps without it:
 if [ ! -h $RTE_SDK/$RTE_TARGET ]; then
     ln -sf $RTE_SDK/build $RTE_SDK/$RTE_TARGET
+fi
+
+########################################
+
+devbind=$(find $RTE_SDK -name 'dpdk-devbind.py' \
+    | head -1)
+
+if [ -f "$devbind" ]; then
+    cp -f $devbind /usr/local/bin
+
+        check_status "failed to copy dpdk-devbind.py"
 fi
 
 ########################################
@@ -167,6 +237,8 @@ conffile="/etc/$pkgname-$version.conf"
     echo "export RTE_SDK=\"$RTE_SDK\"" ; \
     echo "export RTE_TARGET=\"$RTE_TARGET\"" ; \
     echo "export DPDK_VERSION=\"$version\"" ; \
+    echo "export DPDK_DEVBIND=\"$devbind\"" ; \
+    echo "export DPDK_CONFIG=\"$buildconfig\"" ; \
 ) > $conffile
 
 /bin/cp -f $conffile /etc/$pkgname.conf
